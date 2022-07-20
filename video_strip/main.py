e7aa40ea36a9f4c91bdab02dfb7faa54f55e7685
Shaurya K. Panwar <63470280+KlrShaK@users.noreply.github.com>
2022-07-21 00:25:58 +0530

    completed video strip code

--- video_strip/main.py
+++ video_strip/main.py
@@ -1,6 +1,7 @@
 # todo compare same images
 
 import cv2
+from skimage.metrics import structural_similarity as ssim
 
 cap = cv2.VideoCapture('XYZ.avi')
 # For streams:
@@ -9,11 +10,21 @@ cap = cv2.VideoCapture('XYZ.avi')
 #   cap = cv2.VideoCapture(0)
 count = 0
 
+def compare_img(img1, img2):
+    original = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    test = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    if ssim(original, test) < 0.8:
+        return False
+    else:
+        return True
+
+
 while cap.isOpened():
     ret, frame = cap.read()
-
+    prev_frame = None
     if ret:
-        cv2.imwrite('frame{:d}.jpg'.format(count), frame)
+        if not compare_img(frame, prev_frame):
+            cv2.imwrite('frame{:d}.jpg'.format(count), frame)
         count += 100
         cap.set(cv2.CAP_PROP_POS_FRAMES, count)
     else:
